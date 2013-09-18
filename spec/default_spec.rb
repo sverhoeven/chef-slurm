796355413580be8c90fd02afcb787a5aa6e9ea0e
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2013-09-18 10:25:49 +0200

    Add 2 tests.

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -2,7 +2,12 @@ require 'chefspec'
 
 describe 'slurm::default' do
   let (:chef_run) { ChefSpec::ChefRunner.new.converge 'slurm::default' }
-  it 'should do something' do
-    pending 'Your recipe examples go here.'
+
+  it 'installs slurm-llnl' do
+    expect(chef_run).to install_package 'slurm-llnl'
+  end
+
+  it 'creates slurm.conf file' do
+    expect(chef_run).to create_file('/etc/slurm-llnl/slurm.conf')
   end
 end
